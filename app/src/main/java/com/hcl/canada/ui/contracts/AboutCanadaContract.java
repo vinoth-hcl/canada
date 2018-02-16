04edfe1deaa695fc586cd3672aee44d7c986b6a8
Vinoth <rengarajanv@hcl.com>
2018-02-16 16:50:29 +0530

    Added recycler adapter and view holder with view bindings for the UI

--- app/src/main/java/com/hcl/canada/ui/contracts/AboutCanadaContract.java
+++ app/src/main/java/com/hcl/canada/ui/contracts/AboutCanadaContract.java
@@ -5,11 +5,9 @@ import com.hcl.canada.ui.presenters.ViewStatePresenter;
 public class AboutCanadaContract {
 
     public interface Presenter extends ViewStatePresenter<View> {
-
     }
 
     public interface View {
-
     }
 
 }
---
+++ app/src/main/java/com/hcl/canada/ui/presenters/AboutCanadaPresenter.java
@@ -0,0 +1,6 @@
+package com.hcl.canada.ui.presenters;
+
+import com.hcl.canada.ui.contracts.AboutCanadaContract;
+
+public class AboutCanadaPresenter extends BasePresenter<AboutCanadaContract.View> implements AboutCanadaContract.Presenter {
+}
