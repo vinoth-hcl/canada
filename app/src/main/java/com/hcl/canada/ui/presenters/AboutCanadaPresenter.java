85b53ba6ffb692611d9efc099a8cdadbab811458
vinoth-hcl <36504717+vinoth-hcl@users.noreply.github.com>
2018-02-16 17:29:01 +0530

    Revert "List screen ui implementation"

--- app/src/main/java/com/hcl/canada/ui/presenters/AboutCanadaPresenter.java
+++
@@ -1,6 +0,0 @@
-package com.hcl.canada.ui.presenters;
-
-import com.hcl.canada.ui.contracts.AboutCanadaContract;
-
-public class AboutCanadaPresenter extends BasePresenter<AboutCanadaContract.View> implements AboutCanadaContract.Presenter {
-}
